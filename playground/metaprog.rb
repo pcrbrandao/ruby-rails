2cd574c87b00f69e04f535b05f8c2c5c97c3443d
Pedro Brandao <pcrbrandao@gmail.com>
2017-07-05 10:21:13 -0300

    Iniciando Rails

--- playground/metaprog.rb
+++ playground/metaprog.rb
@@ -1,21 +1,64 @@
-class Restaurante
-    def initialize(nome)
-        puts "criando um novo restaurante: #{nome}"
-        @@total ||= 0
-        @@total = @@total + 1
-        Restaurante.relatorio
-        @nome = nome
+module Comercio
+    class Restaurante
+        def initialize(nome)
+            puts "criando um novo restaurante: #{nome}"
+            @@total ||= 0
+            @@total = @@total + 1
+            Restaurante.relatorio
+            @nome = nome
+        end
+
+        def self.relatorio
+            puts "Foram criados #{@@total} restaurantes"
+        end
+
+    end
+end
+
+module Educacao
+    class Aluno
+        #não sabe nada
     end
 
-    def self.relatorio
-        puts "Foram criados #{@@total} restaurantes"
+    class Professor
+        def ensina(aluno)
+            def aluno.escreve
+                puts "sei escrever!"
+            end
+        end
     end
-
 end
 
 def teste_restaurante
-    rest1 = Restaurante.new("Macdonalds")
-    rest2 = Restaurante.new("Comida Chinesa")
+    rest1 = Comercio::Restaurante.new("Macdonalds")
+    rest2 = Comercio::Restaurante.new("Comida Chinesa")
 end
 
+def teste_aluno_nao_sabe_escrever
+    print "teste aluno não sabe escrever... "
+    juca = Educacao::Aluno.new
+    if !juca.respond_to? :escreve
+        puts "não sei escrever :("
+        return
+    end
+    # se ele escreve o teste não passou
+    puts "Sabe escrever?"
+    juca.escreve
+end
+
+def teste_aluno_sabe_escrever
+    print "teste aluno sabe escrever... "
+    juca = Educacao::Aluno.new
+    professor = Educacao::Professor.new
+    professor.ensina juca
+    if !juca.respond_to? :escreve
+        puts "erro... deveria saber escrever"
+        return
+    end
+    #depois que o professor ensina juca sabe escrever
+    juca.escreve
+end
+
+teste_aluno_nao_sabe_escrever
+teste_aluno_sabe_escrever
 teste_restaurante
