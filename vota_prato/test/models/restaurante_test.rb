92dd84ec2b1d796d77f62d2fdff5a71630a45633
Pedro Brandao <pcrbrandao@gmail.com>
2017-07-05 18:16:13 -0300

    Em andamento

--- vota_prato/test/models/restaurante_test.rb
+++ vota_prato/test/models/restaurante_test.rb
@@ -1,10 +1,34 @@
 require 'test_helper'
 
 class RestauranteTest < ActiveSupport::TestCase
-    test "O restaurante deve ser criado" do
+    def cria_um_restaurante
+        Restaurante.destroy_all
         r = Restaurante.new nome: "Fasano", endereco:"End2", especialidade:"Esp2"
         r.save
+        return r
+    end
+
+    def lista_todos_os_restaurantes
+        for r in Restaurante.all
+            puts "restaurante: #{r.nome}"
+        end
+    end
+
+    def test_restaurante_deve_ser_criado
+        r = cria_um_restaurante
+        r1 = Restaurante.where({nome:"Fasano"})
+        lista_todos_os_restaurantes
+        nome1 = r.nome
+        nome2 = r1.nome
+        assert nome1.eql?(nome2), "restaurante #{r.to_s} não é #{r1.to_s}"
+    end
+
+    def test_first_deve_retornar_o_primeiro
+        Restaurante.destroy_all
+        r = cria_um_restaurante
         r1 = Restaurante.first
-        assert !r.eql?(r1), "Restaurante não foi criado"
+        nome1 = r.nome
+        nome2 = r1.nome
+        assert nome1.eql?(nome2), "restaurante #{nome1} não é #{nome2}?"
     end
 end
