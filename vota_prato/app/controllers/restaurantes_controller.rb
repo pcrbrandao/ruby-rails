e7e3abf9ab6961781615d6735ea912a5515bc3d4
Pedro Brandao <pcrbrandao@gmail.com>
2017-07-10 18:25:41 -0300

    Finalizado 9.23

--- vota_prato/app/controllers/restaurantes_controller.rb
+++ vota_prato/app/controllers/restaurantes_controller.rb
@@ -25,4 +25,29 @@ class RestaurantesController < ApplicationController
 
         redirect_to(action: "index")
     end
+
+    def new
+        @restaurante = Restaurante.new
+    end
+
+    def restaurante_params
+        params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+    end
+
+    def create
+        @restaurante = Restaurante.new(restaurante_params)
+        @restaurante.save
+        redirect_to(action: "index")
+    end
+
+    def edit
+        @restaurante = Restaurante.find params[:id]
+    end
+
+    def update
+        @restaurante = Restaurante.find(params[:id])
+        @restaurante.update_attributes(restaurante_params)
+
+        redirect_to action: "index"
+    end
 end
